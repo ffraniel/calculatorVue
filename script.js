af253abc61923dc44ad2d69296b0b2fc440e8f51
ffraniel <francis.whitehead@googlemail.com>
2018-12-13 01:37:04 +0000

    semi functional

--- script.js
+++ script.js
@@ -1,35 +1,85 @@
 var app = new Vue({
   el: '#calculator',
   data: {
-    sum: [],
-    screenNum: 0,
-    screenArray: [],
+    previousNumber: [],
+    currentNumber: [],
+    currentOperator : null,
+    result: null,
   },
   methods: {
     inputNum: function (input) {
-      if (input === '.' && this.screenArray.indexOf('.') === -1) {
-        if (this.screenArray.length === 0) {
-          this.screenArray.push(0);
-          this.screenArray.push(input);
-        } else {
-          this.screenArray.push(input);
+      if (input === '.' && this.currentNumber.indexOf('.') === -1) {
+        if (this.currentNumber.length === 0) {
+          this.currentNumber.push(0);
         }
+        this.currentNumber.push(input);
       } else if (typeof input === 'number') {
-        this.screenArray.push(input);
+        this.currentNumber.push(input);
       }
     },
-    inputDec: function (input) {
-    },
     clearData: function () {
-      this.screenArray = [];
+      this.currentNumber = [];
+      this.previousNumber = [];
+      this.currentOperator = null;
+      this.result = null;
     },
     equals: function (){
+      
+      // if(this.previousNumber.length === 0 || this.currentOperator === null) {
+      //   this.result = this.previousNumber.join("");
+      //   this.resultShowing = true;
+      // }
 
+      let previousNumber = Number(this.previousNumber.join(""));
+      let screenNumber = Number(this.currentNumber.join(""));
+
+      if (this.currentOperator === '+') {
+        this.result = previousNumber + screenNumber;
+      }
+      else if (this.currentOperator === '-') {
+        this.result = previousNumber - screenNumber;
+      }
+      else if (this.currentOperator === '*') {
+        this.result = previousNumber * screenNumber;
+      } 
+      else if (this.currentOperator === '/') {
+        this.result = previousNumber / screenNumber;
+      }
+
+      this.currentNumber = [ Number(this.result)];
+      this.currentOperator = null;
+      this.previousNumber = [];
+    },
+    operator: function (operatorChoice) {
+      let currentNumber = Array.isArray(this.currentNumber) ? Number(this.currentNumber.join("")): this.currentNumber ;
+      this.previousNumber.push(currentNumber);
+
+      if (this.currentOperator === null) {
+        this.currentOperator = operatorChoice;
+        this.currentNumber = [];
+        console.log("operator current: ", this.currentOperator)
+        console.log("current previousNumber ", this.previousNumber);
+
+      } else {
+        // this.previousNumber = [];
+        // this.previousNumber.push(this.result);
+        // this.currentOperator = operatorChoice;
+        // this.currentNumber = [];
+        //call this.equals process
+        (console.log("this.equals() before anything else"))
+        // this.currentOperator = null;
+      }
     },
   },
   computed: {
     displayNumber: function () {
-      return this.screenArray.join("");
+      if(this.previousNumber.length === 0) {
+        return this.currentNumber.join("");
+      } 
+      else {
+        let currentNumberAsVar = Array.isArray(this.currentNumber) ? this.currentNumber.join("") : this.currentNumber;
+        return this.previousNumber.join("").concat(this.currentOperator, currentNumberAsVar);
+      }
     }
   }
 })
